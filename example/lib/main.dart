c9d93ef842d3f2676bb5a53f78397f64f7158517
Michael WU <wwz.michael@gmail.com>
2022-06-20 15:53:56 +0800

    add ios development info and mapbox access token

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -60,7 +60,7 @@ class MapsDemo extends StatefulWidget {
   //
   // Alternatively you can replace `String.fromEnvironment("ACCESS_TOKEN")`
   // in the following line with your access token directly.
-  static const String ACCESS_TOKEN = String.fromEnvironment("ACCESS_TOKEN");
+  static const String ACCESS_TOKEN = "pk.eyJ1IjoibWljaGFlbHd1MDIwNCIsImEiOiJjanVlN2NiOXYwMGppNDRwYmtyN3NtcW1zIn0.gxv7yhHMKu-T_KyHm68j5g";
 
   @override
   State<MapsDemo> createState() => _MapsDemoState();
